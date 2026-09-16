f713ecf06c307a965beb40964e8d082eb60da8a1
chenweijie <fzucwj@gmail.com>
2026-09-16 11:19:53 +0800

    EX6.5 of part6

--- anecdotes/src/components/AnecdoteList.jsx
+++ anecdotes/src/components/AnecdoteList.jsx
@@ -8,7 +8,7 @@ const AnecdoteList = () => {
   }
 	return (
 		<div>
-			{anecdotes.map((anecdote) => (
+			{anecdotes.toSorted((a, b) => b.votes - a.votes).map((anecdote) => (
   		  <div key={anecdote.id}>
   		    <div>{anecdote.content}</div>
   		    <div>
